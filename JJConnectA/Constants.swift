3d86b0d6966254a637820156405f4a9d93fa361e
MangoyeSidney <mangoye@MangoyeSidneys-MacBook-Air.local>
2015-11-27 00:08:19 +0900

    Iniial commit from local git

---
+++ JJConnectA/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  JJConnectA
+//
+//  Created by MangoyeSidney on 11/26/15.
+//  Copyright © 2015 Sidney Mangoye. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
