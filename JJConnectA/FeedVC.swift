2a59ebe60c9c3c62f869957ab94c3639be59a7fa
MangoyeSidney <mangoye@mangoyesidneys-macbook-air.local>
2015-11-28 18:04:48 +0900

    Post UI made and

--- JJConnectA/FeedVC.swift
+++ JJConnectA/FeedVC.swift
@@ -9,10 +9,17 @@
 import UIKit
 import Firebase
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    
+    @IBOutlet weak var postField: MaterialTextField!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageSelectorImage: UIImageView!
+    
     var posts = [Post]()
+    
+    var imagePicker: UIImagePickerController!
+    
     static var imageCache = NSCache()
     
     override func viewDidLoad() {
@@ -22,6 +29,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         tableView.estimatedRowHeight = 358
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
@@ -87,6 +96,16 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             return tableView.estimatedRowHeight
         }
     }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageSelectorImage.image = image
+    }
     
+    @IBAction func selectImage(sender: UITapGestureRecognizer) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func makePost(sender: AnyObject) {
+
+    }
 }
 
--- JJConnectA/MaterialView.swift
+++ JJConnectA/MaterialView.swift
@@ -12,10 +12,10 @@ class MaterialView: UIView {
 
     override func awakeFromNib() {
         layer.cornerRadius = 2.0
-        layer.shadowColor = UIColor(red:SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
-        layer.shadowOpacity = 0.8
-        layer.shadowRadius = 5.0
-        layer.shadowOffset = CGSizeMake (0.0, 2.0)
+        layer.shadowColor = UIColor(red:SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.3).CGColor
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 4.0
+        layer.shadowOffset = CGSizeMake (0.0, 0.0)
     }
 
 }
